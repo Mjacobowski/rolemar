f9193a7245fecc738bd2e9e723f97cd9398d7567
Jacobowski <mjacobowski27@gmail.com>
2022-08-09 15:29:57 -0300

    criado migrates - Categoria - Filiais - UF

---
+++ database/migrations/2022_08_09_000010_create_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->increments('id_cat');
+            $table->string('nome', 45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
---
+++ database/migrations/2022_08_09_000012_create_ufs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ufs', function (Blueprint $table) {
+            $table->increments('id_uf');
+            $table->string('sigla', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ufs');
+    }
+};
